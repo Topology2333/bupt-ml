deca2355faf5f4e436700e15c712e910694dee88
oshimeiko <53026720+butteruni@users.noreply.github.com>
2023-10-06 15:48:25 +0800

    10.6 update framework

---
+++ lab4costModel/evaluation.py
@@ -0,0 +1,39 @@
+import json
+
+from cost_model import estimate_learning
+from evaluation_utils import draw_act_est_figure
+from plan import Plan
+
+
+if __name__ == '__main__':
+    train_json_file = './train_plans.json'
+    test_json_file = './test_plans.json'
+    train_plans, test_plans = [], []
+
+    with open(train_json_file, 'r') as f:
+        train_cases = json.load(f)
+    for case in train_cases:
+        train_plans.append(Plan.parse_plan(case['query'], case['plan']))
+
+    with open(test_json_file, 'r') as f:
+        test_cases = json.load(f)
+    for case in test_cases:
+        test_plans.append(Plan.parse_plan(case['query'], case['plan']))
+
+    act_times = []
+    tidb_costs = []
+    for p in test_plans:
+        act_times.append(p.exec_time_in_ms())
+        tidb_costs.append(p.tidb_est_cost())
+
+    _, _, est_learning_costs, act_learning_times = estimate_learning(train_plans, test_plans)
+
+    draw_act_est_figure("tidb", tidb_costs, act_times, True)
+    draw_act_est_figure("learning", est_learning_costs, act_learning_times, True)
+
+    with open('./results.json', 'w') as outfile:
+        json.dump({
+            "act": act_times,
+            "tidb": tidb_costs,
+            "learning": est_learning_costs,
+        }, outfile)
